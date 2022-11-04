12b438c6ac4a4b7dc676f2c7b12532139d5c5150
Dhruv3sood <dhruvsood6@gmail.com>
2022-11-04 15:34:31 +0530

    First Submission

---
+++ src/Initial.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class Initial {
+
+public static void main(String[] args) {
+    System.out.println("Advanced Programming Project\n");
+    System.out.println("Collaborators:");
+    System.out.println("1.dhruv sood(CSSS)");
+    System.out.println("2.Siddarth(CSSS)");
+    }}
+
